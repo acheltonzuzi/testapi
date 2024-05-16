cc854bfd8043270842f731a15b2023cc930cfd8a
acheltonzuzi <acheltonzuzi@gmail.com>
2024-05-16 15:58:05 -0700

    a

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py  
+from fastapi import FastAPI  
+import uvicorn
+app = FastAPI()
+@app.get('/')
+def home():
+    return {'msg':'hello world'}
+
+if __name__=='__main__':
+    uvicorn.run('main:app',host='0.0.0.0',port=8000)
